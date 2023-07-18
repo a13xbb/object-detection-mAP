b71398ff41a62f2b0a9b910479f2990148a7f53c
a13xbb <alex.blokh24@gmail.com>
2023-07-18 17:42:38 +0300

    refactor + some stats counted

--- map.py
+++ map.py
@@ -4,65 +4,59 @@ from iou import intersection_over_union
 import torch
 from matplotlib import pyplot as plt
 import numpy as np
+import seaborn as sns
 
-def mean_average_precision(data: pd.DataFrame, iou_threshold=0.4, plot_curves=False):
-    '''Calculates mAP for each type of object based on dataframe'''
+def get_metrics (data: pd.DataFrame, obj_type: str, iou_threshold=0.4) -> tuple:
+    '''Calculates mAP for each type of object based on dataframe
+       Returns AP, number of true objects, number of TP samples, number of FP samples,
+       precisions, recalls'''
     epsilon = 1e-9
     class_list = data['obj_type'].unique()
-    APs = {}
+    if obj_type not in class_list:
+        raise Exception('No such obj_type in passed dataframe')
     
-    #preparing axes for plots
-    nrows = int(np.ceil(len(class_list) / 3))
-    ncols = 3
-    fig, axs = plt.subplots(nrows, ncols, figsize=(15, 10))
-    for i in range(nrows):
-        for j in range(ncols):
-            axs[i, j].set_visible(False)
-    ax_x = 0
-    ax_y = 0
+    cur_class_df = data[data['obj_type'] == obj_type]
+    cur_class_df = cur_class_df.sort_values('confidence', ascending=False)
+    num_detections = len(cur_class_df.loc[(cur_class_df['w_pred'] != 0) & (cur_class_df['h_pred'] != 0)])
+    num_true_objects = len(cur_class_df.loc[(cur_class_df['w_gt'] != 0) & (cur_class_df['h_gt'] != 0)])
     
-    for type in class_list:
-        cur_class_df = data[data['obj_type'] == type]
-        cur_class_df = cur_class_df.sort_values('confidence', ascending=False)
-        num_detections = len(cur_class_df.loc[(cur_class_df['w_pred'] != 0) & (cur_class_df['h_pred'] != 0)])
-        num_true_objects = len(cur_class_df.loc[(cur_class_df['w_gt'] != 0) & (cur_class_df['h_gt'] != 0)])
-        
-        TP = torch.zeros(num_detections)
-        FP = torch.zeros(num_detections)
-        
-        detection_idx = 0
-        for index, row in cur_class_df.iterrows():
-            if row['w_pred'] != 0 and row['h_pred'] != 0:
-                cords_pred = [row['x_pred'], row['y_pred'], row['w_pred'], row['h_pred']]
-                cords_gt = [row['x_gt'], row['y_gt'], row['w_gt'], row['h_gt']]
-                iou = intersection_over_union(cords_pred, cords_gt)
-                if iou >= iou_threshold:
-                    TP[detection_idx] = 1
-                else:
-                    FP[detection_idx] = 1
-                
-                detection_idx += 1
-                
-        TP_cumsum = torch.cumsum(TP, dim=0)
-        FP_cumsum = torch.cumsum(FP, dim=0)
-        
-        recalls = TP_cumsum / (num_true_objects + epsilon)
-        precisions = TP_cumsum / (TP_cumsum + FP_cumsum + epsilon)
-        recalls = torch.cat((torch.tensor([0]), recalls))
-        precisions = torch.cat((torch.tensor([1]), precisions))
-        
-        APs[type] = float(torch.trapz(precisions, recalls))
-        
-        #plotting current class AP curve
-        axs[ax_x, ax_y].set_visible(True)
-        axs[ax_x, ax_y].plot(recalls, precisions, label=f'AP = {round(APs[type], 4)}')
-        axs[ax_x, ax_y].set_title(type)
-        axs[ax_x, ax_y].legend(loc='upper right')
-        axs[ax_x, ax_y].set_xlabel('recall')
-        axs[ax_x, ax_y].set_ylabel('precision')
-        ax_y += 1
-        if ax_y == 3:
-            ax_y = 0
-            ax_x += 1
+    TP = torch.zeros(num_detections)
+    FP = torch.zeros(num_detections)
+    
+    detection_idx = 0
+    for index, row in cur_class_df.iterrows():     
+        if row['w_pred'] != 0 and row['h_pred'] != 0:
+            cords_pred = [row['x_pred'], row['y_pred'], row['w_pred'], row['h_pred']]
+            cords_gt = [row['x_gt'], row['y_gt'], row['w_gt'], row['h_gt']]
+            iou = intersection_over_union(cords_pred, cords_gt)
+            if iou >= iou_threshold:
+                TP[detection_idx] = 1
+            else:
+                FP[detection_idx] = 1
+            
+            detection_idx += 1
+            
+    TP_cumsum = torch.cumsum(TP, dim=0)
+    FP_cumsum = torch.cumsum(FP, dim=0)
+    
+    recalls = TP_cumsum / (num_true_objects + epsilon)
+    precisions = TP_cumsum / (TP_cumsum + FP_cumsum + epsilon)
+    recalls = torch.cat((torch.tensor([0]), recalls))
+    precisions = torch.cat((torch.tensor([1]), precisions))
+    
+    AP = float(torch.trapz(precisions, recalls))
+    TP_cnt = TP_cumsum[-1]
+    FP_cnt = FP_cumsum[-1]
 
-    return APs
+    return AP, num_true_objects, num_detections, int(TP_cnt), int(FP_cnt), precisions, recalls
+
+
+def plot_pr_curve(precisions, recalls, title) -> None:
+    AP = float(torch.trapz(precisions, recalls))
+    plt.plot(recalls, precisions, label=f'AP = {round(AP, 4)}')
+    plt.title(title)
+    plt.legend(loc='upper right')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.show()
+    return
